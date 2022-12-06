fca3f534412ca5ebc26819f4073f782cf6169512
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-12-06 16:00:28 -0500

    Adds latest changes

--- js/script.js
+++ js/script.js
@@ -16,6 +16,10 @@ playerImage.src = 'shadow_dog.png'
 const spriteWidth = 575
 // 5230px of height / 10 anitations or 10 rows = 523
 const spriteHeight = 523
+let frameX = 0
+let frameY = 0
+let gameFrame = 0
+const staggerFrames = 5
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
@@ -24,15 +28,23 @@ function animate() {
   // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
   ctx.drawImage(
     playerImage,
-    0,
-    0,
+    frameX * spriteWidth,
+    frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
     0,
     0,
-    CANVAS_WIDTH,
-    CANVAS_HEIGHT
+    spriteWidth,
+    spriteHeight
   )
+
+  if (gameFrame % staggerFrames == 0) {
+    if (frameX < 6) frameX++
+    else frameX = 0
+  }
+
+  gameFrame++
+
   requestAnimationFrame(animate)
 }
 
