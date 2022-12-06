2d5ea95224c2afb11de4850a96bbc465d2c22ccf
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-12-06 15:43:09 -0500

    Adds latest changes

--- js/script.js
+++ js/script.js
@@ -10,13 +10,29 @@ const CANVAS_WIDTH = (canvas.width = 600)
 const CANVAS_HEIGHT = (canvas.height = 600)
 
 const playerImage = new Image()
-playerImage.src = 'assets/img/CarrotStickman/skeleton-animation_00.png'
-let x = 0
+// playerImage.src = 'assets/img/CarrotStickman/skeleton-animation_00.png'
+playerImage.src = 'shadow_dog.png'
+// 6876px of width / 12 columns = 573
+const spriteWidth = 575
+// 5230px of height / 10 anitations or 10 rows = 523
+const spriteHeight = 523
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.fillRect(100, 50, 100, 100)
-  ctx.drawImage(playerImage, 0, 0)
+  // ctx.fillRect(100, 50, 100, 100)
+  // ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+  // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+  ctx.drawImage(
+    playerImage,
+    0,
+    0,
+    spriteWidth,
+    spriteHeight,
+    0,
+    0,
+    CANVAS_WIDTH,
+    CANVAS_HEIGHT
+  )
   requestAnimationFrame(animate)
 }
 
