12bb88109f69b80cda4b9d075518067946dfb9cc
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-12-06 15:10:57 -0500

    Adds latest changes

--- js/script.js
+++ js/script.js
@@ -11,10 +11,12 @@ const CANVAS_HEIGHT = (canvas.height = 600)
 
 const playerImage = new Image()
 playerImage.src = 'assets/img/CarrotStickman/skeleton-animation_00.png'
+let x = 0
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  ctx.fillRect(50, 50, 100, 100)
+  ctx.fillRect(x, 50, 100, 100)
+  x++
   requestAnimationFrame(animate)
 }
 
