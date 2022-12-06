63b96b6ce278f46bba8f78d5b25d5a8862847622
Jesiel Palacios <jopalacioss@unac.edu.co>
2022-12-06 18:06:19 -0500

    Adds latest changes

--- js/script.js
+++ js/script.js
@@ -1,34 +1,24 @@
-// Canvas
 const canvas = document.getElementById('canvas1')
-
-// Context
 const ctx = canvas.getContext('2d')
-console.log('ctx', ctx)
-
-// Sizes
 const CANVAS_WIDTH = (canvas.width = 600)
 const CANVAS_HEIGHT = (canvas.height = 600)
 
 const playerImage = new Image()
-// playerImage.src = 'assets/img/CarrotStickman/skeleton-animation_00.png'
 playerImage.src = 'shadow_dog.png'
-// 6876px of width / 12 columns = 573
-const spriteWidth = 575
-// 5230px of height / 10 anitations or 10 rows = 523
-const spriteHeight = 523
+const spriteWidth = 575 // 6876px of width / 12 columns = 573
+const spriteHeight = 523 // 5230px of height / 10 animations or 10 rows = 523
 let frameX = 0
-let frameY = 5
+let frameY = 0
 let gameFrame = 0
 const staggerFrames = 5
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  // ctx.fillRect(100, 50, 100, 100)
-  // ctx.drawImage(playerImage, 0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
-  // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh)
+  let position = Math.floor(gameFrame / staggerFrames) % 6
+  frameX = spriteWidth * position
   ctx.drawImage(
     playerImage,
-    frameX * spriteWidth,
+    frameX,
     frameY * spriteHeight,
     spriteWidth,
     spriteHeight,
@@ -37,11 +27,6 @@ function animate() {
     spriteWidth,
     spriteHeight
   )
-
-  if (gameFrame % staggerFrames == 0) {
-    if (frameX < 4) frameX++
-    else frameX = 0
-  }
 
   gameFrame++
 
